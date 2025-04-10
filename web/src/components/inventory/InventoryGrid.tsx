35376e5d1465be7dd0b8ceb344df3c67ba1e88e4
Pokjad Rafaey <161868365+pokjadrafaey@users.noreply.github.com>
2025-04-10 15:32:05 +0800

    Update InventoryGrid.tsx

--- web/src/components/inventory/InventoryGrid.tsx
+++ web/src/components/inventory/InventoryGrid.tsx
@@ -6,7 +6,7 @@ import { getTotalWeight } from '../../helpers';
 import { useAppSelector } from '../../store';
 import { useIntersection } from '../../hooks/useIntersection';
 
-const PAGE_SIZE = 30;
+const PAGE_SIZE = 105;
 
 const InventoryGrid: React.FC<{ inventory: Inventory }> = ({ inventory }) => {
   const weight = useMemo(
